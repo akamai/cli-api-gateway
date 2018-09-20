2265180623473d022e76b5b664655b58bc59aff2
Johanna Cherry <johanna@johannacherry.com>
2018-09-20 15:39:08 -0500

    Add output helper functions for json and table formats, add flags to list command

--- command-bootstrap.go
+++ command-bootstrap.go
@@ -15,10 +15,14 @@
 package main
 
 import (
-	api "github.com/johannac/AkamaiOPEN-edgegrid-golang/api-endpoints-v2"
-	// api "github.com/akamai/AkamaiOPEN-edgegrid-golang/configdns-v1"
-	akamai "github.com/akamai/cli-common-golang"
+	"encoding/json"
+	"fmt"
 
+	akamai "github.com/akamai/cli-common-golang"
+	api "github.com/johannac/AkamaiOPEN-edgegrid-golang/api-endpoints-v2"
+
+	"github.com/fatih/color"
+	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
 )
 
@@ -48,5 +52,61 @@ func initConfig(c *cli.Context) error {
 		return err
 	}
 	api.Init(config)
+	return nil
+}
+
+func outputStruct(c *cli.Context, structToReturn interface{}, err error) error {
+	j, err := json.Marshal(structToReturn)
+	if err != nil {
+		akamai.StopSpinnerFail()
+		return cli.NewExitError(color.RedString(err.Error()), 1)
+	}
+
+	sr := [][]string{}
+	err = json.Unmarshal(j, sr)
+
+	if err != nil {
+		akamai.StopSpinnerFail()
+		return cli.NewExitError(color.RedString(err.Error()), 1)
+	}
+
+	if c.IsSet("json") && c.Bool("json") {
+		return outputStructToJSON(c, sr, err)
+	}
+
+	return outputStructToTable(c, sr, err)
+}
+
+func outputStructToJSON(c *cli.Context, structToReturn [][]string, err error) error {
+	if err != nil {
+		akamai.StopSpinnerFail()
+		return cli.NewExitError(color.RedString(err.Error()), 1)
+	}
+
+	returnJSON, err := json.MarshalIndent(structToReturn, "", "  ")
+	if err != nil {
+		akamai.StopSpinnerFail()
+		return cli.NewExitError(color.RedString(err.Error()), 1)
+	}
+
+	akamai.StopSpinnerOk()
+	fmt.Fprintln(c.App.Writer, string(returnJSON))
+	return nil
+}
+
+func outputStructToTable(c *cli.Context, structToReturn [][]string, err error) error {
+	if err != nil {
+		akamai.StopSpinnerFail()
+		return cli.NewExitError(color.RedString(err.Error()), 1)
+	}
+
+	table := tablewriter.NewWriter(c.App.Writer)
+	table.SetReflowDuringAutoWrap(false)
+	table.SetAutoWrapText(false)
+	table.SetRowLine(true)
+	table.AppendBulk(structToReturn)
+	// TODO: Add header
+	akamai.StopSpinnerOk()
+	table.Render()
 	return nil
 }
--- command-list-endpoints.go
+++ command-list-endpoints.go
@@ -16,44 +16,93 @@ package main
 
 import (
 	"fmt"
-	// "os"
 
+	akamai "github.com/akamai/cli-common-golang"
 	api "github.com/johannac/AkamaiOPEN-edgegrid-golang/api-endpoints-v2"
-	// api "github.com/akamai/AkamaiOPEN-edgegrid-golang/configdns-v1"
 
 	"github.com/fatih/color"
 	"github.com/urfave/cli"
 )
 
+var flagsListEndpoints *api.ListEndpointOptions = &api.ListEndpointOptions{}
+
 var commandListEndpoints cli.Command = cli.Command{
 	Name:        "list-endpoints",
-	ArgsUsage:   "<record type> <hostname>",
+	ArgsUsage:   "",
 	Description: "Retrieve a list of APIs running through the Gateway; may be used as input to other commands",
 	HideHelp:    true,
 	Action:      callListEndpoints,
-	Subcommands: cli.Commands{},
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "json",
+			Usage: "Output JSON format",
+		},
+		cli.StringFlag{
+			Name:        "contract",
+			Usage:       "Filters endpoints to a specific contract. You need to specify this along with a groupId.",
+			Destination: &flagsListEndpoints.ContractId,
+		},
+		cli.IntFlag{
+			Name:        "group",
+			Usage:       "Filters endpoints to a specific group. You need to specify this along with a contractId.",
+			Destination: &flagsListEndpoints.GroupId,
+		},
+		cli.StringFlag{
+			Name:        "show",
+			Usage:       "The type of endpoints to return based on their visibility status. By default the API returns ALL endpoints. You can instead decide to return ONLY_VISIBLE endpoints, or ONLY_HIDDEN endpoints.",
+			Destination: &flagsListEndpoints.Show,
+		},
+		cli.StringFlag{
+			Name:        "version-preference",
+			Usage:       "The preference for picking the endpoint version to return. By default the API returns the LAST_UPDATED version. If you set the preference to ACTIVATED_FIRST, the API first attempts to return the version currently active on the production network. If such version doesn’t exist, the API attempts to return the version currently active on the staging network. If both of these checks fail, the API returns the last updated version.",
+			Destination: &flagsListEndpoints.VersionPreference,
+		},
+		cli.StringFlag{
+			Name:        "sortBy",
+			Usage:       "The field to sort endpoints by, either the API name (corresponding to the apiEndPointName member) or updateDate.",
+			Destination: &flagsListEndpoints.SortBy,
+		},
+		cli.StringFlag{
+			Name:        "sortOrder",
+			Usage:       "The sort order, either desc for descending or the default asc for ascending.",
+			Destination: &flagsListEndpoints.SortOrder,
+		},
+		cli.StringFlag{
+			Name:        "contains",
+			Usage:       "The search query substring criteria matching the endpoint’s name, description, basePath, apiCategoryName, and resourcePath.",
+			Destination: &flagsListEndpoints.Contains,
+		},
+		cli.StringFlag{
+			Name:        "category",
+			Usage:       "Filters endpoints by the specified apiCategoryName, including the __UNCATEGORIZED__ keyword.",
+			Destination: &flagsListEndpoints.Category,
+		},
+		cli.IntFlag{
+			Name:        "page-size",
+			Usage:       "The number of endpoints on each page of results, 25 by default.",
+			Destination: &flagsListEndpoints.PageSize,
+		},
+		cli.IntFlag{
+			Name:        "page",
+			Usage:       "The page number index, starting at the default value of 1.",
+			Destination: &flagsListEndpoints.Page,
+		},
+	},
 }
 
 func callListEndpoints(c *cli.Context) error {
-	fmt.Println(color.YellowString("Hello World"))
-
 	err := initConfig(c)
 	if err != nil {
 		return cli.NewExitError(color.RedString(err.Error()), 1)
 	}
 
-	list := api.EndpointList{}
-	err = list.GetEndpointsList(api.ListEndpointOptions{
-		ContractId: "ctr_C-1FRYVV3",
-		GroupId:    "grp_68817",
-		Page:       5,
-		PageSize:   50,
-	})
-	// zone, err := api.GetZone("akamaideveloper.net")
-	if err != nil {
-		return cli.NewExitError(color.RedString(err.Error()), 1)
-	}
-	// fmt.Println(color.BlueString(fmt.Sprintf("%s", zone)))
-	fmt.Println(color.BlueString("Goodbye World"))
-	return nil
+	akamai.StartSpinner(
+		"Fetching endpoints list...",
+		fmt.Sprintf("Fetching endpoints list...... [%s]", color.GreenString("OK")),
+	)
+
+	list := &api.EndpointList{}
+	err = list.GetEndpointsList(flagsListEndpoints)
+
+	return outputStruct(c, list.APIEndPoints, err)
 }
